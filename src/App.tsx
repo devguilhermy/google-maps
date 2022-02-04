5905ddd68aca872acc3af5255d0902b720b66e41
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2022-02-04 11:22:49 -0300

    feature: directions with waypoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Map } from './components/Map';
+import { Itinerary, Map, MapOptions } from './components/Map';
 import { Marker } from './components/Marker';
 import { Status, Wrapper } from '@googlemaps/react-wrapper';
 import { useState } from 'react';
@@ -9,21 +9,55 @@ export default function App() {
     };
 
     const [clicks, setClicks] = useState<google.maps.LatLng[]>([]);
-    const [zoom, setZoom] = useState(3); // initial zoom
-    const [center, setCenter] = useState<google.maps.LatLngLiteral>({
-        lat: -15.9,
-        lng: 46.9,
+    // const [zoom, setZoom] = useState(3); // initial zoom
+    // const [center, setCenter] = useState<google.maps.LatLngLiteral>();
+
+    const [options, setOptions] = useState<MapOptions>({
+        zoom: 12,
+        center: {
+            lat: -16.6,
+            lng: -49.2,
+        },
     });
+
+    const [itinerary, setItinerary] = useState<Itinerary>({} as Itinerary);
+
+    // const useEffect(()=>{}, [clicks])
 
     const onClick = (e: google.maps.MapMouseEvent) => {
         // avoid directly mutating state
-        setClicks([...clicks, e.latLng!]);
+
+        // setClicks([...clicks, e.latLng!]);
+
+        if (!itinerary.origin) {
+            setItinerary({ ...itinerary, origin: e.latLng! });
+            return;
+        }
+
+        if (itinerary.origin && !itinerary.destination) {
+            setItinerary({ ...itinerary, destination: e.latLng! });
+            return;
+        }
+
+        if (itinerary.origin && itinerary.destination) {
+            const newItinerary = { ...itinerary };
+            const newWaypoints = newItinerary.waypoints || [];
+            setItinerary({
+                ...itinerary,
+                destination: e.latLng!,
+                waypoints: [
+                    ...newWaypoints,
+                    { location: itinerary.destination, stopover: true },
+                ],
+            });
+            return;
+        }
     };
 
     const onIdle = (m: google.maps.Map) => {
-        console.log('onIdle');
-        setZoom(m.getZoom()!);
-        setCenter(m.getCenter()!.toJSON());
+        // console.log('onIdle');
+        // setZoom(m.getZoom()!);
+        // setCenter(m.getCenter()!.toJSON());
     };
 
     return (
@@ -33,14 +67,20 @@ export default function App() {
                 render={render}
             >
                 <Map
-                    options={{ zoom, center }}
+                    options={options}
+                    setOptions={setOptions}
                     className="h-screen"
                     onClick={onClick}
                     onIdle={onIdle}
+                    itinerary={itinerary}
                 >
-                    {clicks.map((click) => {
-                        return <Marker position={click} />;
-                    })}
+                    {/* {clicks.map((click, i) => {
+                        return <Marker position={click} key={i} />;
+                    })} */}
+                    {itinerary.origin && !itinerary.destination && (
+                        <Marker position={itinerary.origin} />
+                    )}
+                    ;
                 </Map>
             </Wrapper>
         </div>
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import useDeepCompareEffect from 'use-deep-compare-effect';
 import {
 	ReactNode,
@@ -7,32 +7,52 @@ import {
 	useState
 	} from 'react';
 
+export interface Itinerary {
+    origin?: google.maps.LatLng;
+    destination?: google.maps.LatLng;
+    waypoints?: google.maps.DirectionsWaypoint[];
+}
+
+export interface MapOptions {
+    center: google.maps.LatLngLiteral;
+    zoom: number;
+}
+
 interface MapProps {
-    options: {
-        center: google.maps.LatLngLiteral;
-        zoom: number;
-    };
     className: string;
+    options: MapOptions;
+    setOptions?: (options: MapOptions) => void;
     onClick?: (e: google.maps.MapMouseEvent) => void;
     onIdle?: (map: google.maps.Map) => void;
     children?: ReactNode;
+    itinerary: Itinerary;
 }
 
 export function Map({
-    options,
     className,
+    options,
+    setOptions,
     onClick,
     onIdle,
     children,
+    itinerary,
 }: MapProps) {
     const ref = useRef<HTMLDivElement>(null);
     const [map, setMap] = useState<google.maps.Map>();
 
+    const memoizedDirectionsService = useMemo(() => {
+        return new google.maps.DirectionsService();
+    }, []);
+
+    const memoizedDirectionsRenderer = useMemo(() => {
+        return new google.maps.DirectionsRenderer();
+    }, []);
+
     useEffect(() => {
         if (ref.current && !map) {
-            setMap(new window.google.maps.Map(ref.current, {}));
+            setMap(new window.google.maps.Map(ref.current, options));
         }
-    }, [ref, map]);
+    }, [ref, map, options]);
 
     useDeepCompareEffect(() => {
         if (map) {
@@ -42,6 +62,8 @@ export function Map({
 
     useEffect(() => {
         if (map) {
+            memoizedDirectionsRenderer.setMap(map);
+
             ['click', 'idle'].forEach((eventName) =>
                 google.maps.event.clearListeners(map, eventName)
             );
@@ -53,8 +75,41 @@ export function Map({
             if (onIdle) {
                 map.addListener('idle', () => onIdle(map));
             }
+
+            // map.addListener(
+            //     'center_changed',
+            //     (e: google.maps.LatLngLiteral) => {
+            //         console.log(e);
+            //         setOptions &&
+            //             setOptions({
+            //                 ...options,
+            //                 center: { lat: e.lat, lng: e.lng },
+            //             });
+            //     }
+            // );
         }
-    }, [map, onClick, onIdle]);
+    }, [map, onClick, onIdle, options, setOptions, memoizedDirectionsRenderer]);
+
+    useEffect(() => {
+        if (itinerary.origin && itinerary.destination) {
+            const request: google.maps.DirectionsRequest = {
+                origin: itinerary.origin,
+                destination: itinerary.destination,
+                travelMode: google.maps.TravelMode.DRIVING,
+                waypoints: itinerary.waypoints,
+                optimizeWaypoints: false,
+                provideRouteAlternatives: false,
+            };
+
+            console.log('request', request);
+
+            memoizedDirectionsService.route(request, function (result, status) {
+                if (status === 'OK') {
+                    memoizedDirectionsRenderer.setDirections(result);
+                }
+            });
+        }
+    }, [itinerary, memoizedDirectionsService, memoizedDirectionsRenderer]);
 
     return (
         <>
